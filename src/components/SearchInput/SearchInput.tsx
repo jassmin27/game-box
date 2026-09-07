41ed626d2ac75eaf74e8ada58bbc355504a079f1
Jasmine Kaur <jassmin27@gmail.com>
2026-09-07 16:57:21 +0100

    Fix search from game detail page

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -2,16 +2,19 @@ import { IoSearchOutline } from "react-icons/io5";
 import styles from "./SearchInput.module.css";
 import { useRef } from "react";
 import useGameQueryStore from "../../store";
+import { useNavigate } from "react-router";
 
 function SearchInput() {
+  const navigate = useNavigate();
   const ref = useRef<HTMLInputElement>(null);
-  const setSearchText = useGameQueryStore(s => s.setSearchText);
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
 
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         setSearchText(ref.current?.value.trim() || null);
+        navigate("/");
       }}
     >
       <div className={styles["search-group"]}>
